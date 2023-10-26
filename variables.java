d94847af01c0a1bbbb665f1f6afecd67515f8754
LakshyaAgrawal127001 <boypc3539@gmail.com>
2023-10-27 01:41:37 +0530

    Variables code

---
+++ variables.java
@@ -0,0 +1,14 @@
+public class variables {
+    
+    public static void main(String[]args){
+       System.out.println("Variables");
+
+       String name = "Java";
+       String program = "Developer";
+       String whatDe = program;
+
+       System.out.println(name);
+       System.out.println(whatDe);
+
+    }
+}
